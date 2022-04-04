1f5f588e7398b7ba4511a0da5d408d30fb3582e2
Forofonof <DanielResult1@outlook.com>
2022-04-04 12:31:13 +0300

    Добавьте файлы проекта.

---
+++ Homework_09/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        int numbers;
+        int maximumNumber = 97;
+        int numberStep = 7;
+
+        for (numbers = 5; numbers < maximumNumber; numbers += numberStep)
+        {
+            Console.WriteLine(numbers);
+        }
+    }
+}
